d5b0654686f3834b714aa43bc26df666f300aaf2
A_D <aunderscored@gmail.com>
2022-02-20 05:17:41 +0200

    add good and bad homeserver counts to status

--- bot/commands.go
+++ bot/commands.go
@@ -40,11 +40,13 @@ func (b *Bot) manualScan(a *chatcommand.Argument) error {
 	return nil
 }
 
-func (b *Bot) scanStatus() (cacheSize, inProgress, completed int) {
+func (b *Bot) scanStatus() (cacheSize, inProgress, completed, good, bad int) {
 	b.mu.Lock()
 	cacheSize = len(b.cache)
 	inProgress = 0
 	completed = 0
+
+	good, bad = 0, 0
 
 	for _, v := range b.cache {
 		switch v.state {
@@ -52,20 +54,26 @@ func (b *Bot) scanStatus() (cacheSize, inProgress, completed int) {
 			inProgress++
 		case scanComplete:
 			completed++
+			if v.isOpenReg {
+				bad++
+			} else {
+				good++
+			}
 		}
 	}
 	defer b.mu.Unlock()
 
-	return cacheSize, inProgress, completed
+	return cacheSize, inProgress, completed, good, bad
 }
 
 func (b *Bot) statuscmd(a *chatcommand.Argument) error {
-	cacheSize, progress, completed := b.scanStatus()
+	cacheSize, progress, completed, good, bad := b.scanStatus()
 
 	a.Replyf(
 		"Bot status: Version \x02%s\x02 | \x02%d\x02 goroutines | \x02%d\x02 cached homeservers | "+
-			"\x02%d\x02 scans in progress | \x02%d\x02 scans completed | cache entries cleared after \x02%d\x02 hours",
-		b.config.Version, runtime.NumGoroutine(), cacheSize, progress, completed, b.config.ScanTimeoutHours,
+			"\x02%d\x02 scans in progress | \x02%d\x02 scans completed (G: %d | B: %d) |"+
+			" cache entries cleared after \x02%d\x02 hours",
+		b.config.Version, runtime.NumGoroutine(), cacheSize, progress, completed, good, bad, b.config.ScanTimeoutHours,
 	)
 
 	return nil
