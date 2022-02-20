1b63bb597ed75cdf17d301ac351b168ff216dc18
A_D <aunderscored@gmail.com>
2022-02-20 02:42:02 +0200

    drop init method

--- main.go
+++ main.go
@@ -11,15 +11,13 @@ import (
 	"github.com/pelletier/go-toml/v2"
 )
 
-func init() {
+func main() {
 	//nolint:lll,misspell // cannot be shortened, and I'd love to spell correctly but the dev was apparently american.
 	const format = "%{color}[%{time:15:04:05.0000}] [%{level: -8s}]%{color:reset} [%{module: -15s}] [%{shortfile: -15s}] %{message}"
 
 	logging.SetFormatter(logging.MustStringFormatter(format))
 	logging.SetBackend(logging.NewLogBackend(os.Stdout, "", 0))
-}
 
-func main() {
 	var config bot.Config
 
 	data, err := os.ReadFile("./config.toml")
